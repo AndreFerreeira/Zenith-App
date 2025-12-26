6ce9159f880addd20a52ed87643fc6afa5667bb5
Andre Ferreira <and.ferreeira@gmail.com>
2025-12-26 12:20:13 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider, type Auth } from 'firebase/auth';
 
-// SUBSTITUA OS VALORES ABAIXO PELOS DO SEU PROJETO FIREBASE
+// Configuração do seu projeto Firebase
 export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "COLE_SEU_AUTH_DOMAIN_AQUI",
-  projectId: "COLE_SEU_PROJECT_ID_AQUI",
-  storageBucket: "COLE_SEU_STORAGE_BUCKET_AQUI",
-  messagingSenderId: "COLE_SEU_MESSAGING_SENDER_ID_AQUI",
-  appId: "COLE_SEU_APP_ID_AQUI"
+  apiKey: "AIzaSyA0gflRnyzShnAIQrM0gl4VYc_UAXBDFfk",
+  authDomain: "habitos-zenith.firebaseapp.com",
+  projectId: "habitos-zenith",
+  storageBucket: "habitos-zenith.appspot.com",
+  messagingSenderId: "329968976274",
+  appId: "1:329968976274:web:ed0852ae45d60a2f16c14b",
+  measurementId: "G-EXV93TJH1W"
 };
 
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
