a83b0c3ae2ca40d37b72effe14a94860a84be163
Andre Ferreira <and.ferreeira@gmail.com>
2025-12-26 11:20:38 +0000

    Esse erro **"Cannot read properties of undefined (reading 'trimEnd')"**

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // NOTE: This is a placeholder configuration.
 // When a Firebase project is properly connected, these values will be filled in automatically.
 export const firebaseConfig = {
-  apiKey: "YOUR_EXACT_API_KEY_FROM_CONSOLE",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyC21T8V4JmZ7s5r3f2d1e0a9b8c7d6e5",
+  authDomain: "zenith-mastery-app.firebaseapp.com",
+  projectId: "zenith-mastery-app",
+  storageBucket: "zenith-mastery-app.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:a1b2c3d4e5f6a1b2c3d4e5"
 };
